614a59319f263f1bf7c471a9bd3401a12d609a20
dmullandev <dwmullan@gmail.com>
2022-08-21 21:01:30 +0100

    correctly converted pricing-service to microservice. Added eureka server.

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,14 +2,14 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * 
  */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
